59a68e2965a3c6bb1c48c6409164c26977d38f5d
hehj <hehj@mama.cn>
2023-06-20 23:31:54 +0800

    fix: 请求加载提示

--- src/common/socketio.ts
+++ src/common/socketio.ts
@@ -1,9 +1,8 @@
-import { io } from "socket.io-client";
-const socketHost = 'http://127.0.0.1:3000/'
-
+import {io} from 'socket.io-client';
+const socketHost = process.env.VITE_PORT as string;
 const socketIo = io(socketHost, {
-  path: '', // 如果地址是wss path:"/wss"
-  transports: ['websocket', 'xhr-polling', 'jsonp-polling'],
-  autoConnect: true,   // 自动连接     
-})
-export default socketIo
+    path: '', // 如果地址是wss path:"/wss"
+    transports: ['websocket', 'xhr-polling', 'jsonp-polling'],
+    autoConnect: true // 自动连接
+});
+export default socketIo;
